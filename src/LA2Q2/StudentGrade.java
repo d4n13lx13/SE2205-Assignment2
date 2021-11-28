39bf7b56000fc5db788a761dc16d64ce12f1beba
Piotr Nowak <piotr_2001@hotmail.com>
2021-11-27 20:37:38 -0500

    Stuff
    Hi

--- src/LA2Q2/StudentGrade.java
+++ src/LA2Q2/StudentGrade.java
@@ -1,7 +1,34 @@
 package LA2Q2;
 
-public class StudentGrade implements Comparable<StudentGrade>{
+public class StudentGrade implements Comparable<StudentGrade>
+{
+    public StudentGrade(String firstName, String lastName, int grade)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
 
-    //Stuff here
+    private String firstName, lastName;
+    private int grade;
 
+    public void setFirstName(String name)
+    {
+        this.firstName = name;
+    }
+
+    public void setLastName(String name)
+    {
+        this.lastName = name;
+    }
+
+    public void setGrade(int grade)
+    {
+        this.grade = grade;
+    }
+
+    @Override
+    public int compareTo(StudentGrade o) {
+        return 0;
+    }
 }
