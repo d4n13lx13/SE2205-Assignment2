59725b8168dd8027ced97d43c344eed99aae3b30
formatspecifiernotfound <danxie2001@gmail.com>
2021-11-27 21:25:57 -0500

    driver method

--- src/LA2Q1/DanielMichaelTestingSortingMethods.java
+++ src/LA2Q1/DanielMichaelTestingSortingMethods.java
@@ -7,6 +7,63 @@ public class DanielMichaelTestingSortingMethods {
 
     //Stuff here
 
+    public static <T extends Comparable<? super T>> long selectionSort(T[] a) {
+        long start, finish;
+        start = System.nanoTime();
+        if(a.length < 2)
+            return 0;
+        for (int i = 0; i < a.length; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[smallest].compareTo(a[j]) > 0) {
+                    smallest = j;
+                }
+            }
+            T temp = a[i];
+            a[i] = a[smallest];
+            a[smallest] = temp;
+        }
+        finish = System.nanoTime();
+        return finish - start;
+    }
+
+    public static <T extends Comparable<? super T>> long bubbleSort(T[] a) {
+        long start, finish;
+        start = System.nanoTime();
+        if(a.length < 2)
+            return 0;
+        for (int i = a.length - 1; i > 0; i--) {
+            for (int j = 0; (j + 1) <= i; j++) {
+                if(a[j].compareTo(a[j+1]) > 0) {
+                    T temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        finish = System.nanoTime();
+        return finish - start;
+    }
+
+    public static < T extends Comparable <? super T >> long insertionSort(T[] a) {
+        long start, finish;
+        start = System.nanoTime();
+        if(a.length < 2)
+            return 0;
+
+        for(int i = 0; i < a.length; i++) {
+            for(int j = i; j > 0; j--) {
+                if(a[j].compareTo(a[j - 1]) < 0) {
+                    T temp = a[j];
+                    a[j] = a[j - 1];
+                    a[j - 1] = temp;
+                }
+            }
+        }
+
+        return System.nanoTime() - start;
+    }
+
 
     public static long bucketSort(Integer[] a, int first, int last, int maxDigits) {
         long start = System.nanoTime();
--- src/LA2Q2/DriverClass.java
+++ src/LA2Q2/DriverClass.java
@@ -1,5 +1,10 @@
 package LA2Q2;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Vector;
+
 public class DriverClass {
     public static void main(String[] args) {
 //call your header method
@@ -10,14 +15,32 @@ public class DriverClass {
                 "Draco", "Dean", "Fred"};
         String[] lnArray = {"Granger", "Weasley", "Potter", "Lovegood",
                 "Weasley", "Malfoy", "Thomas", "Weasley"};
-        Integer[] grd = {(int)(60 + Math.random()*26),(int)(60 +
-                Math.random()*26),(int)(60 + Math.random()*26),(int)(60 +
-                Math.random()*26),(int)(60 + Math.random()*26),(int)(60 +
-                Math.random()*26),(int)(60 + Math.random()*26),(int)(60 +
-                Math.random()*26)};
+        Integer[] grd = {(int) (60 + Math.random() * 26), (int) (60 +
+                Math.random() * 26), (int) (60 + Math.random() * 26), (int) (60 +
+                Math.random() * 26), (int) (60 + Math.random() * 26), (int) (60 +
+                Math.random() * 26), (int) (60 + Math.random() * 26), (int) (60 +
+                Math.random() * 26)};
+
+        header();
+
+        Vector<StudentGrade> sg = new Vector<>();
+
+        for (int i = 0; i < fnArray.length; i++) {
+            sg.add(new StudentGrade(fnArray[i], lnArray[i], grd[i]));
+        }
+
 //create a Vector<> class instance sg with StudentGrade tag.
 //Add each StudentGrade object to the Vector class with its add method (Slide #36, Unit 2 Part 2)
+
+
+
 //print the unsorted sg contents just by using toString() method (see the sample output)
+
+        System.out.println("Unsorted Array: ");
+        for (int i = 0; i < sg.size(); i++) {
+            System.out.printf("%s %s :  ");
+        }
+
 //sort sg using Collections’ sort method and that will sort the list based on the grade
 //print the sorted content by using toString() method (see the sample output)
 
@@ -36,6 +59,28 @@ public class DriverClass {
 // Call your sort method again with the argument of StudentGrade array (you created above) and key = 2.
 //print the sorted array (see the sample output)
 //call your footer method here
+
+        footer();
+    }
+
+    public static void header() {
+        System.out.println("*****************************************************************************************************" +
+                "\nNames: Daniel and Michael" +
+                "\nStudent Numbers: 251076275 and 251110243" +
+                "\nGoal of this project: To sort an array of students by their grades, first names, and last names."
+                "\n*****************************************************************************************************");
+    }
+
+    public static void footer() {
+        LocalTime currentTime = LocalTime.now();
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter dt1 = DateTimeFormatter.ofPattern("hh:mm");
+
+        System.out.println("**********************************************************" +
+                "\nThis is " + currentTime.format(dt1) + " on " + currentDate +
+                "\nCompletion of Lab Assignment 2 is successful!" +
+                "\nGood bye! Daniel Xie and Michael Wong" +
+                "\n**********************************************************");
     }
 //Your sort method will go here.
 
